f1e3cf7b8889ca10448845b48ac7d9094092c7d4
Craig Lane <craig.lane@pubnub.com>
2022-04-28 15:32:29 -0700

    Initial Commit

---
+++ pubnub-issue-file-upload/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pubnub-issue-file-upload
+//
+//  Created by Craig Lane on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
